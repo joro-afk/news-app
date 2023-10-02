995be0c8ebf3332d92f566bced7583cef651b71e
Joaquin <joro2509@gmail.com>
2023-10-03 00:05:22 +0200

    03/10

--- src/components/stores/newsStore.js
+++ src/components/stores/newsStore.js
@@ -1,59 +1,59 @@
-import { defineStore } from 'pinia';
-import axios from 'axios';
+// import { defineStore } from 'pinia';
+// import axios from 'axios';
 
-export const useNewsStore = defineStore({
-    id: "newsStore",
-    state() {
-        return {
-            data: '',
-            news: '',
-            location: '',
-            category: '',
-        }
-    },
+// export const useNewsStore = defineStore({
+//     id: "newsStore",
+//     state() {
+//         return {
+//             data: '',
+//             news: '',
+//             location: '',
+//             category: '',
+//         }
+//     },
 
-    actions: {
-        async setNews() {
-            const options = {
-                method: 'POST',
-                url: 'https://newsnow.p.rapidapi.com/newsv2',
-                headers: {
-                    'content-type': 'application/json',
-                    'X-RapidAPI-Key': '869dd0e0bfmsh0cce425d64a68b4p159db5jsn25def7f3b22f',
-                    'X-RapidAPI-Host': 'newsnow.p.rapidapi.com'
-                },
-                data: {
-                    query: 'AI',
-                    page: 3,
-                    time_bounded: true,
-                    from_date: '30/09/2023',
-                    to_date: '01/10/2023',
-                    location: this.location,
-                    category: this.category,
-                    source: ''
-                }
-            };
+//     actions: {
+//         async setNews() {
+//             const options = {
+//                 method: 'POST',
+//                 url: 'https://newsnow.p.rapidapi.com/newsv2',
+//                 headers: {
+//                     'content-type': 'application/json',
+//                     'X-RapidAPI-Key': '869dd0e0bfmsh0cce425d64a68b4p159db5jsn25def7f3b22f',
+//                     'X-RapidAPI-Host': 'newsnow.p.rapidapi.com'
+//                 },
+//                 data: {
+//                     query: 'AI',
+//                     page: 3,
+//                     time_bounded: true,
+//                     from_date: '30/09/2023',
+//                     to_date: '01/10/2023',
+//                     location: this.location,
+//                     category: this.category,
+//                     source: ''
+//                 }
+//             };
 
-            try {
-                const response = await axios.request(options);
-                this.data = response.data;
-                this.news = response.data.news;
-                console.log(this.location + 1)
-                console.log(this.category + 1)
-            } catch (error) {
-                console.error(error);
-            }
-        },
-        updateInputValues(newValue1, newValue2) {
-            this.location = newValue1;
-            this.category = newValue2;
-            console.log(this.location)
-            console.log(this.category)
-            this.setNews()
-        },
-
-
-    },
+//             try {
+//                 const response = await axios.request(options);
+//                 this.data = response.data;
+//                 this.news = response.data.news;
+//                 console.log(this.location + 1)
+//                 console.log(this.category + 1)
+//             } catch (error) {
+//                 console.error(error);
+//             }
+//         },
+//         updateInputValues(newValue1, newValue2) {
+//             this.location = newValue1;
+//             this.category = newValue2;
+//             console.log(this.location)
+//             console.log(this.category)
+//             this.setNews()
+//         },
 
 
-})
+//     },
+
+
+// })
