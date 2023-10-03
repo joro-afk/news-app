849183347a3265f2d26141de90700155e4e9e406
Joaquin <joro2509@gmail.com>
2023-10-03 18:39:28 +0200

    0310

--- src/components/stores/newsStore.js
+++ src/components/stores/newsStore.js
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia';
-// import axios from 'axios';
+import axios from 'axios';
 
 export const useNewsStore = defineStore({
     id: "newsStore",
@@ -13,35 +13,35 @@ export const useNewsStore = defineStore({
     },
 
     actions: {
-        // async setNews() {
-        //     const options = {
-        //         method: 'POST',
-        //         url: 'https://newsnow.p.rapidapi.com/newsv2',
-        //         headers: {
-        //             'content-type': 'application/json',
-        //             'X-RapidAPI-Key': '869dd0e0bfmsh0cce425d64a68b4p159db5jsn25def7f3b22f',
-        //             'X-RapidAPI-Host': 'newsnow.p.rapidapi.com'
-        //         },
-        //         data: {
-        //             query: 'AI',
-        //             page: 3,
-        //             time_bounded: true,
-        //             from_date: '25/09/2023',
-        //             to_date: '01/10/2023',
-        //             location: this.location,
-        //             category: this.category,
-        //             source: ''
-        //         }
-        //     };
+        async setNews() {
+            const options = {
+                method: 'POST',
+                url: 'https://newsnow.p.rapidapi.com/newsv2',
+                headers: {
+                    'content-type': 'application/json',
+                    'X-RapidAPI-Key': '869dd0e0bfmsh0cce425d64a68b4p159db5jsn25def7f3b22f',
+                    'X-RapidAPI-Host': 'newsnow.p.rapidapi.com'
+                },
+                data: {
+                    query: 'AI',
+                    page: 3,
+                    time_bounded: true,
+                    from_date: '25/09/2023',
+                    to_date: '01/10/2023',
+                    location: this.location,
+                    category: this.category,
+                    source: ''
+                }
+            };
 
-        //     try {
-        //         const response = await axios.request(options);
-        //         this.data = response.data;
-        //         this.news = response.data.news;
-        //     } catch (error) {
-        //         console.error(error);
-        //     }
-        // },
+            try {
+                const response = await axios.request(options);
+                this.data = response.data;
+                this.news = response.data.news;
+            } catch (error) {
+                console.error(error);
+            }
+        },
         updateInputValues(newValue1, newValue2) {
             this.location = newValue1;
             this.category = newValue2;
