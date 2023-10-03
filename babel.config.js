6362673d3a431654ed89fc9b6ef37b63389063c6
Joaquin <joro2509@gmail.com>
2023-10-03 14:30:38 +0200

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   presets: [
     [
-      "@vue/app", 
+      '@vue/cli-plugin-babel/preset', 
       { 
         targets: { esmodules: true },
-        polyfills: false
+        polyfills: []
       }
     ]
   ]
